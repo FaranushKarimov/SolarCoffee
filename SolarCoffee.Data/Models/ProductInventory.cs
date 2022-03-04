93cd93b44379b4c69e7c19464d33200f93ca7cf0
FaranushKarimov <faranush.karimov@gmail.com>
2022-03-04 15:03:54 +0300

    Create other models and run migration

---
+++ SolarCoffee.Data/Models/ProductInventory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarCoffee.Data.Models
+{
+    public class ProductInventory
+    {
+        public int Id { get; set; }
+        public DateTime CreateOn { get; set; }
+        public DateTime UpdateOn { get; set; }
+        public int QuantityOnHand { get; set; }
+        public int IdealQuantity { get; set; }
+        public Product Product { get; set; }
+    }
+}
---
+++ SolarCoffee.Data/Models/SalesOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarCoffee.Data.Models
+{
+    public class SalesOrder
+    {
+        public int Id { get; set; }
+        public DateTime CreateOn { get; set; }
+        public DateTime UpdateOn { get; set; }
+        public Customer Customer { get; set; }
+        public List<SalesOrderItem> SalesOrderItems { get; set; }
+        public bool IsPaid { get; set; }
+    }
+}
